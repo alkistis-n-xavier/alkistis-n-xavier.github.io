537b12bd8ce9d34832d59e51c11432977f846d22
xsoriano <xavinos@alum.mit.edu>
2019-09-29 23:50:41 -0400

    Desktop Layout added with simple decision code

---
+++ src/mobile-decision.js
@@ -0,0 +1,22 @@
+var mobileRe = "/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/";
+
+var siteHome = "alkistis-n-xavier.github.io/"
+var locParts = document.location.href.split(siteHome);
+var loc = locParts[locParts.length - 1];
+
+var desktopSites = {
+	'save-the-date.html' : 'index.html'
+}
+
+var mobileSites = {
+	'' : 'save-the-date.html',
+	'index.html' : 'save-the-date.html'
+}
+
+if (navigator.userAgent.match(mobileRe)) {
+	if (desktopSites.hasOwnProperty(loc)) document.location = desktopSites[loc];
+} else{
+	if (mobileSites.hasOwnProperty(loc)) document.location = mobileSites[loc];
+}
+
+
